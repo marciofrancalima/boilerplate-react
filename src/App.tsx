5c7c6aa4287a9c8ea39b69230349ba9b8371fb78
Márcio França Lima <marciofrancalima@hotmail.com>
2020-09-09 20:06:10 -0300

    Add test coverage and update lint-staged config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
+import Main from './pages/Main'
+
 function App() {
-  return <h1>Hello World</h1>
+  return <Main />
 }
 
 export default App
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Main = () => (
+  <main>
+    <h1>Hello World</h1>
+  </main>
+)
+
+export default Main
